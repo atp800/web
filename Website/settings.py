9f21157cb35572a19545079a656c401ca324ec7b
Andreas Panayiotou (BA/BSc Liberal Arts + Sciences) <atp800@student.bham.ac.uk>
2024-01-05 01:54:36 +0000

    Added routing for index file

--- Website/settings.py
+++ Website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,7 +58,7 @@ ROOT_URLCONF = "Website.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'frontend/dist')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,3 +127,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = 'users.User'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/dist/static'),
+]
--- Website/urls.py
+++ Website/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('users/', include('users.urls')),
     path('posts/', include('posts.urls')),
     path('', include('home.urls')),
 ]
+
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path('users/', include('users.urls')),
+#     path('posts/', include('posts.urls')),
+#     path('', include('home.urls')),
+# ]
